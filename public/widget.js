2123a7a60f66cf40017646f3ae27bde3ee0f627e
MeBs2004 <bharatsharma3399@gmail.com>
2026-06-17 17:09:23 +0530

    Added widget script

--- public/widget.js
+++ public/widget.js
@@ -3,19 +3,17 @@
   const iframe = document.createElement("iframe");
 
   iframe.src =
-    "https://nuform-chatbot-frontend2-git-main-mebs2004s-projects.vercel.app";
+    "https://nuform-chatbot-frontend2.vercel.app";
 
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
-  iframe.style.width = "370px";
-  iframe.style.height = "560px";
-  iframe.style.border = "3px solid red";
+  iframe.style.width = "380px";
+  iframe.style.height = "600px";
+  iframe.style.border = "none";
   iframe.style.zIndex = "999999";
-  iframe.style.background = "white";
+  iframe.style.background = "transparent";
 
   document.body.appendChild(iframe);
-
-  console.log("Nuform Widget Loaded");
 
 })();
